86a2b7dcdb8cab87a28fe90eaa015b66a92636ea
Michael Medis <TheMedisGroup@gmail.com>
2015-10-14 19:48:46 -0400

    Assignment 20 completed

---
+++ greeting.rb
@@ -0,0 +1,10 @@
+	def greeting 
+ 		first, second, third, fourth = ARGV
+ 		salutation = ARGV[0]	
+ 		puts "#{salutation} #{second}"
+ 		puts "#{salutation} #{third}"
+ 		puts "#{salutation} #{fourth}"
+ 	end
+ 	greeting
+
+
