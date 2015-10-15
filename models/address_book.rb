5ae9f3cc96414758d23b703113c371da358fa7a2
Michael Medis <TheMedisGroup@gmail.com>
2015-10-15 12:22:45 -0400

    Checkpoint 23 completed, csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
 	attr_accessor :entries
@@ -8,7 +9,6 @@ class AddressBook
 	end
 
 	def add_entry(name, phone_number, email)
-
 		index = 0
 		@entries.each do |entry|
 
@@ -20,8 +20,18 @@ class AddressBook
 
 		@entries.insert(index, Entry.new(name, phone_number, email))
 	end
+
+	def import_from_csv(file_name)
+		csv_text = File.read(file_name)
+		csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+		csv.each do |row|
+		row_hash = row.to_hash
+		add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+	end
+end
 	def remove_entry(name, phone_number, email)
-		 
+
 		remove = 0
 		@entries.each do |entry|
 
@@ -30,7 +40,7 @@ class AddressBook
 			end
 			remove = entry
 		end
-		
+
 		@entries.delete(remove)
 	end
 end
